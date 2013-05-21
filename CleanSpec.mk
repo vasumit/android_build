6b70defa695d6940d0dade9a3aa1b383cfe5e564
Cristiano Matos <cristianobmatos@gmail.com>
2013-05-21 20:31:43 -0300

    Linaro 4.7
    Added Linaro

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -152,6 +152,13 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/*)
 
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 
+# For GCC 4.7 toolchain change
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/STATIC_LIBRARIES)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/EXECUTABLES)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/lib/*.o)
+
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
